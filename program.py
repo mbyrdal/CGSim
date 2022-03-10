e97ae841c57e4ebd2ce41da99b78cc4d3e6ca9a6
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2022-03-10 19:22:44 +0100

    Finalized program.py
    Added JSON and dict configuration

--- program.py
+++ program.py
@@ -1,7 +1,5 @@
-# Simulates a corrupted gauntlet run and its resulting rewards
-# Reward chest contains three regular drop rolls, including shards and a roll on the unique table
-
 import random
+import pprint
 
 # Item IDs
 regdrops = {0 : "Rune full helm", 1 : "Rune chainbody", 2 : "Rune platebody", 
@@ -18,85 +16,123 @@ quantity = [(3,5),(2,3),(2,2),(2,3),(2,3),(2,3),(2,3),
                 (175,250),(125,150),(100,150),(200,350),
                 (125,175),(100,150),(1000,1500),(500,725),
                 (250,450),(50,100),(25,65),(15,60),(10,40),(5,15),(8,12)]
-quantityspecial = [(75000,150000),(1,2)]
+quantity_special = [(75000,150000),(1,2)]
 
 # Prices
-prices = {0 : 20508, 1 : 29461, 2 : 38410, 3 : 37754,
-          4 : 37858, 5 : 37612, 6 : 18682, 7 : 123,
-	      8 : 189, 9 : 140, 10 : 60, 11 : 190,
-	      12 : 304, 13 : 5, 14 : 26, 15 : 55,
-	      16 : 1292, 17 : 368, 18 : 572, 19 : 1135,
-	      20 : 2796, 21 : 8098}
-pricesspecial = {22 : 1, 23 : 148936}
+price = {0 : 20508, 1 : 29461, 2 : 38410, 3 : 37754,
+         4 : 37858, 5 : 37612, 6 : 18682, 7 : 123,
+	     8 : 189, 9 : 140, 10 : 60, 11 : 190,
+	     12 : 304, 13 : 5, 14 : 26, 15 : 55,
+	     16 : 1292, 17 : 368, 18 : 572, 19 : 1135,
+	     20 : 2796, 21 : 8098}
+price_special = {22 : 1, 23 : 148936}
 
-def randomizeamount(item_id):
+# Generates a list of random number rolls (1 to 48) to be used in ID finding
+# 0-indexed as [0, 1, 2]
+def get_roll():
+    random_rolls = []
+    for i in range(0,3):
+        n = random.randint(1,48)
+        random_rolls.append(n)
+    return random_rolls
+
+# Generates the quantity of a specific item with item_id
+# Integer value
+def get_quantity(item_id):
     if item_id == 22:
-        q = random.randint(quantityspecial[0][0], quantityspecial[0][1])
+        q = random.randint(quantity_special[0][0], quantity_special[0][1])
     elif item_id == 23:
-        q = random.randint(quantityspecial[1][0], quantityspecial[1][1])
+        q = random.randint(quantity_special[1][0], quantity_special[1][1])
     else:
         q = random.randint(quantity[item_id][0], quantity[item_id][1])
     return q
 
-def calculateprofit(item_id, quantity):
+# Generates the price of a specific item with item_id and quantity
+# Integer value
+def get_profit(item_id, quantity):
     if item_id == 22 or item_id == 23:
-        p = quantity * pricesspecial.get(item_id)
+        p = quantity * price_special.get(item_id)
     else:
-        p = quantity * prices.get(item_id)
+        p = quantity * price.get(item_id)
     return p
-
-def finditem(numlist):
-    for x in range(0, 3, 1):
-        if 1 <= numlist[x] >= 44:
-            roll_val = numlist[x]
-            id_val = random.randint(0,21)
-            name_val = regdrops.get(id_val)
-            q_val = randomizeamount(id_val)
+    
+# Generates a dictionary object based on the rolls (regular drop)
+def get_object(numlist):
+    for x in range(0,3,1):
+        if 1 <= numlist[x] <= 44:
+            roll_value = numlist[x]
+            id_value = random.randint(0,21)
+            name_value = regdrops.get(id_value)
+            quantity_value = get_quantity(id_value)
+            profit_value = get_profit(id_value, quantity_value)
         elif 45 <= numlist[x] <= 47:
-            roll_val = numlist[x]
-            id_val = 22
-            name_val = otherdrops.get(id_val)
-            q_val = randomizeamount(id_val)
+            roll_value = numlist[x]
+            id_value = 22
+            name_value = otherdrops.get(id_value)
+            quantity_value = get_quantity(id_value)
+            profit_value = get_profit(id_value, quantity_value)
         else:
-            roll_val = numlist[x]
-            id_val = 23
-            name_val = otherdrops.get(id_val)
-            q_val = randomizeamount(id_val)
-    return {"id": id_val, "name": name_val, "quantity": q_val, "roll": roll_val}
+            roll_value = numlist[x]
+            id_value = 23
+            name_value = otherdrops.get(id_value)
+            quantity_value = get_quantity(id_value)
+            profit_value = get_profit(id_value, quantity_value)
+    return {"id": id_value, "name": name_value, 
+            "quantity": quantity_value, "profit": profit_value, "roll": roll_value}    
 
-def findtertiary():
+# Generates a dictionary object based on the rolls (uniques)
+def get_tertiary():
     temp = {}
-    tertiary = random.randint(1, 800)
+    tertiary = random.randint(1,800)
     if 1 <= tertiary <= 759:
-        temp = {"roll": tertiary, "tname": "None", "tquantity": 1, "tprofit": 0}
+        roll_t = tertiary
+        name_t = "None"
+        quantity_t = 1
+        profit_t = 0
     elif 760 <= tertiary <= 783:
-        temp = {"roll": tertiary, "name": "Clue scroll (elite)", "tquantity": 1, "tprofit": 0}
+        roll_t = tertiary
+        name_t = "Clue scroll (elite)"
+        quantity_t = 1
+        profit_t = 0
     elif 784 <= tertiary <= 797:
-        if random.randint(1, 2) == 1:
-            temp = {"troll": tertiary, "tname": "Crystal weapon seed", "tquantity": 1, "tprofit": 337976}
+        if random.randint(1,2) == 1:
+            roll_t = tertiary
+            name_t = "Crystal weapon seed"
+            quantity_t = 1
+            profit_t = 337976
         else:
-            temp = {"troll": tertiary, "tname": "Crystal armour seed", "tquantity": 1, "tprofit": 6825541}
+            roll_t = tertiary
+            name_t = "Crystal armour seed"
+            quantity_t = 1
+            profit_t = 6825541
     elif 798 <= tertiary <= 799:
-        temp = temp = {"troll": tertiary, "tname": "Enhanced crystal weapon seed", "quantity": 1, "tprofit": 162804290}
+        roll_t = tertiary
+        name_t = "Enhanced crystal weapon seed"
+        quantity_t = 1
+        profit_t = 162804290
     else:
-        temp = {"troll": tertiary, "tname": "Yungllef", "tquantity": 1, "tprofit": 0}
+        roll_t = tertiary
+        name_t = "Yungllef"
+        quantity_t = 1
+        profit_t = 0
+    temp.update({"name_t": name_t, "quantity_t": quantity_t,
+            "profit_t": profit_t, "roll_t": roll_t})
     return temp
 
-def reward():
+# Compiles the program and runs it in order to generate reward chest loot + potential tertiary loot
+def run():
     result = {}
-    rolls = []
-    for i in range(0, 3):
-        n = random.randint(1, 48)
-        rolls.append(n)
+    temp_list = get_roll()
     
-    result.update(finditem(rolls))
+    result.update(get_object(temp_list))
     result.update({"crystals": random.randint(5,9)})
-    tertiary = findtertiary()
-    result.update(tertiary)
+    
+    tertiary = get_tertiary()
+    result["tertiary"] = tertiary
+    
     return result
 
-mylist = []
-for z in range(5):
-    mylist.append(reward())
-
-print(mylist[0])
+completions = []
+for x in range(3):
+    completions.append(run())
+pprint.pprint(completions)
