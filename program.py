a97efc737c661bba089eccb48a4433e059e0cd54
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2022-03-10 11:11:19 +0100

    Updated program
    Added item quantities

--- program.py
+++ program.py
@@ -3,14 +3,32 @@
 
 import random
 
-# 'Regular' drops
-regdrops = {1 : "Rune full helm", 2 : "Rune chainbody", 3 : "Rune platebody", 
-            4 : "Rune platelegs", 5 : "Rune plateskirt", 6 : "Rune halberd",
-            7 : "Rune pickaxe", 8 : "Cosmic rune", 9 : "Nature rune",
-            10 : "Law rune", 11 : "Chaos rune", 12 : "Death rune",
-            13 : "Blood rune", 14 : "Mithril arrow", 15 : "Adamant arrow",
-            16 : "Rune arrow", 17 : "Dragon arrow", 18 : "Uncut sapphire",
-            19 : "Uncut emerald", 20 : "Uncut ruby", 21 : "Uncut diamond", 22 : "Battlestaff"}
+# Item IDs
+regdrops = {0 : "Rune full helm", 1 : "Rune chainbody", 2 : "Rune platebody", 
+            3 : "Rune platelegs", 4 : "Rune plateskirt", 5 : "Rune halberd",
+            6 : "Rune pickaxe", 7 : "Cosmic rune", 8 : "Nature rune",
+            9 : "Law rune", 10 : "Chaos rune", 11 : "Death rune",
+            12 : "Blood rune", 13 : "Mithril arrow", 14 : "Adamant arrow",
+            15 : "Rune arrow", 16 : "Dragon arrow", 17 : "Uncut sapphire",
+            18 : "Uncut emerald", 19 : "Uncut ruby", 20 : "Uncut diamond", 21 : "Battlestaff"}
+otherdrops = {22 : "Coins", 23 : "Dragon halberd"}
+
+# Quantity ranges
+quantity = [(3,5),(2,3),(2,2),(2,3),(2,3),(2,3),(2,3),
+                (175,250),(125,150),(100,150),(200,350),
+                (125,175),(100,150),(1000,1500),(500,725),
+                (250,450),(50,100),(25,65),(15,60),(10,40),(5,15),(8,12)]
+quantityspecial = [(75000,150000),(1,2)]
+
+def randomizeamount(item_id):
+    if item_id == 22:
+        q = random.randint(quantityspecial[0][0], quantityspecial[0][1])
+    elif item_id == 23:
+        q = random.randint(quantityspecial[1][0], quantityspecial[1][1])
+    else:
+        q = random.randint(quantity[item_id][0], quantity[item_id][1])
+    return q
+
 
 def droptable():
     randomlist = []
@@ -18,16 +36,19 @@ def droptable():
         n = random.randint(1, 48)
         randomlist.append(n)
 
+    # Name (Quantity): Roll number
+
     for x in range(0, 3, 1):
         if 1 <= randomlist[x] <= 44:
-            print(f"{regdrops.get(random.randint(1,22))}: {randomlist[x]}")
+            rand = random.randint(0,21)
+            print(f"{regdrops.get(rand)} ({randomizeamount(rand)}): {randomlist[x]}")
         elif 45 <= randomlist[x] <= 47:
-            print(f"Coins: {randomlist[x]}")
+            print(f"{otherdrops.get(22)} ({randomizeamount(22)}): {randomlist[x]}")
         else:
-            print(f"Dragon halberd: {randomlist[x]}")
+            print(f"{otherdrops.get(23)} ({randomizeamount(23)}): {randomlist[x]}")
     
     crystals = random.randint(5,9)
-    print(f"Crystal shards: {crystals}")
+    print(f"Crystal shards ({crystals})")
 
     tertiary = random.randint(1, 800)
     if 1 <= tertiary <= 759:
@@ -44,23 +65,6 @@ def droptable():
         print(f"Enhanced crystal weapon seed ({tertiary})")
     else:
         print(f"Yungllef ({tertiary})")
-
-    # def findEnhanced():
-        # while True:
-            # tertiary = random.randint(1, 800)
-            # print(tertiary)
-            # if rand in (798, 799):
-                # break
-
-    # def findYungllef():
-        # while True:
-            # tertiary = random.randint(1, 800)
-            # print(tertiary)
-            # if rand == 800:
-                # break
-
-    # findEnhanced()
-    # findYungllef()
 
 for z in range(3):
     droptable()
